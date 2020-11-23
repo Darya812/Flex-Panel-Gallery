a81491bc7317b3520df509545366e46abfcba754
Darya Radzevich <ganocij1@gmail.com>
2020-11-23 10:57:25 +0300

    fix: add functions, styles, content

--- action.js
+++ action.js
@@ -1,15 +1,16 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll(".panel");
 
-    function toggleOpen() {
-              this.classList.toggle('open');
-    }
+function toggleOpen() {
+  this.classList.toggle("open");
+}
 
-    function toggleActive(e) {
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
 
-      if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-      }
-    }
-
-    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
